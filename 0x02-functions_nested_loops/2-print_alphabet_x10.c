ce1563988ec4950ec175927f2ea3de385b88d0e4
natty-2445 <natalietben@gmail.com>
2023-07-18 16:38:22 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - print alphabet 10 times
+  */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int count = 1;
+
+	do {
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+
+		count++;
+
+		_putchar('\n');
+	} while (count < 11);
+
+
+}
