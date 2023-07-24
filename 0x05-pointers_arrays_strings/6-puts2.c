dba5233bf248997ca38c1741260fac5c09da3358
natty-2445 <natalietben@gmail.com>
2023-07-24 15:01:50 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *puts2 - prints every other character of a string
+  *@str: the string
+  *Return: nothing
+  */
+
+void puts2(char *str)
+{
+	int i;
+	int length = 0;
+
+	for  (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		_putchar(str[i]);
+
+	}
+
+	_putchar('\n');
+
+}
